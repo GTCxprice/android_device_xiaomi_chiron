26bcff504030b771b279c02190ec67817f28bcee
Shreyas(GTCxprice) <92420047+GTCxprice@users.noreply.github.com>
2023-07-02 12:41:32 +0530

    Update Cheetah fingerprint

--- lineage_chiron.mk
+++ lineage_chiron.mk
@@ -39,8 +39,8 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Fingerprint
-BUILD_FINGERPRINT := google/cheetah/cheetah:13/TQ2A.230405.003.E1/9802792:user/release-keys
-BUILD_DESCRIPTION := cheetah-user 13 TQ2A.230405.003.E1 9802792 release-keys
+BUILD_FINGERPRINT := google/cheetah/cheetah:13/TQ3A.230605.012/10204971:user/release-keys
+BUILD_DESCRIPTION := cheetah-user 13 TQ3A.230605.012 10204971 release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
